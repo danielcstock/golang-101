8f738a05949a2063e770f6948df35160d248dee5
danielstock <danielcstock12@gmail.com>
2023-01-22 20:56:26 -0300

    golang-101 aula 4

--- go/src/hello/hello.go
+++ go/src/hello/hello.go
@@ -2,27 +2,27 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
 func main() {
 	showIntro()
 
-	showMenu()
-
-	command := scanCommand()
-	switch command {
-	case 1:
-		fmt.Println("Monitorando...")
-	case 2:
-		fmt.Println("Exibindo logs...")
-	default:
-		fmt.Println("Comando desconhecido")
-		os.Exit(-1)
+	for {
+		showMenu()
+		command := scanCommand()
+		switch command {
+		case 1:
+			startMonitoring()
+		case 2:
+			fmt.Println("Exibindo logs...")
+		default:
+			fmt.Println("Finalizando execução...")
+			os.Exit(0)
+		}
 	}
 
-	fmt.Println("Finalizando execução...")
-	os.Exit(0)
 }
 
 func showIntro() {
@@ -42,4 +42,15 @@ func scanCommand() int {
 func showMenu() {
 	fmt.Println("1 - Iniciar monitoramento")
 	fmt.Println("2 - Exibier logs")
+}
+func startMonitoring() {
+	fmt.Println("Monitorando...")
+	site := "https://random-status-code.herokuapp.com/"
+	response, _ := http.Get(site)
+	if response.StatusCode == 200 {
+		fmt.Println("Site", site, "foi carregado com sucesso.")
+	} else {
+		fmt.Println("Site", site, "está com problemas. Status code", response.Status)
+	}
+
 }
