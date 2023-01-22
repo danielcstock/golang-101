1ef6f1842938b299d6c7653dd4ce6a4b0c7e0e45
danielstock <danielcstock12@gmail.com>
2023-01-22 14:31:49 -0300

    reflect.TypeOf() test

--- go/src/hello/hello.go
+++ go/src/hello/hello.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	name := "Stock"
+	version := 1.1
+	fmt.Println("Olá,", name)
+	fmt.Println("Este programa está na versão", version)
+
+	fmt.Println("O tipo da variável 'name' é:", reflect.TypeOf(name))
+	fmt.Println("O tipo da variável 'version' é:", reflect.TypeOf(version))
 }
