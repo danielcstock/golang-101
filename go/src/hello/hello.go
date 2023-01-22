eda7f1ffd235dcdd77364f6b8b54a82eee3be790
danielstock <danielcstock12@gmail.com>
2023-01-22 20:35:33 -0300

    golang-101 aula 3

--- go/src/hello/hello.go
+++ go/src/hello/hello.go
@@ -1,26 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	showIntro()
 
-	fmt.Println("1 - Iniciar monitoramento")
-	fmt.Println("2 - Exibier logs")
+	showMenu()
 
-	var comando int
-	fmt.Scan(&comando)
-
-	switch comando {
+	command := scanCommand()
+	switch command {
 	case 1:
 		fmt.Println("Monitorando...")
 	case 2:
 		fmt.Println("Exibindo logs...")
 	default:
 		fmt.Println("Comando desconhecido")
+		os.Exit(-1)
 	}
 
 	fmt.Println("Finalizando execução...")
+	os.Exit(0)
 }
 
 func showIntro() {
@@ -28,4 +30,16 @@ func showIntro() {
 	version := 1.1
 	fmt.Println("Olá,", name)
 	fmt.Println("Este programa está na versão", version)
+}
+
+func scanCommand() int {
+	var command int
+	fmt.Scan(&command)
+
+	return command
+}
+
+func showMenu() {
+	fmt.Println("1 - Iniciar monitoramento")
+	fmt.Println("2 - Exibier logs")
 }
